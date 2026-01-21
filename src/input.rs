30744d105881db486e52cc1ec65784339cc3af89
Денис Загороднев <gladiator9000@inbox.ru>
2026-01-22 00:35:33 +0300

    исправляем код get_input_number

--- src/input.rs
+++ src/input.rs
@@ -16,13 +16,25 @@ pub fn get_number(promt: &str) -> f64 {
     input_user
 }
 
-pub fn get_input_number(promt: &str) -> Result<f64, String> {
-    print!("{}", promt);
+pub fn get_input_number(prom: &str) -> Result<f64, String> {
+    print!("{}", prom);
     let mut input_user = String::new();
-    io::stdout().flush().expect("Ошибка высвобождения буфера"); // Сброс буфера вывода
+
+    // stdout().flush() - Сброс буфера вывода
+    io::stdout().flush().expect("Ошибка высвобождения буфера");
     io::stdin().read_line(&mut input_user).expect("Error input user...");
-    
-    input_user.trim().parse::<f64>().map_err(|_| format!("Error number {}", input_user.trim()))
+
+    // Создаем новую переменную типа Result и присваеваем ей input_user
+    let input_number = input_user.trim().parse::<f64>();
+    match input_number {
+        Ok(number) => Ok(number),
+        Err(err) => {
+            /* Тк input_user остался у нас строкой мы можем его вывести в ошибку!
+                макрос format! делает тоже самое что и println!, но вместо вывода возращает сторку!
+            */
+            Err(format!("Ошибка ввода, {} не является числом! {}", input_user.trim(), err))
+        }
+    }
 }
 
 /*
